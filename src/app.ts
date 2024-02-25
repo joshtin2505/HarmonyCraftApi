18104ab226fa56ad1f488ca646febc1ea5a439b4
joshtin2505 <justincastro2505@gmail.com>
2024-02-25 11:58:57 -0500

    Add Randomizer routes to app

--- src/app.ts
+++ src/app.ts
@@ -1,9 +1,11 @@
 import express from 'express'
 import noteRoutes from './routes/Harmony.routes'
+import RandomizerRoutes from './routes/Randomizer.routes'
 
 const app = express()
 
 app.use(express.json())
+app.use('/api/random', RandomizerRoutes)
 app.use('/api', noteRoutes)
 
 export default app
