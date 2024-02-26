d90e1e1da2e3e1475d5c44bf333c4331dd269c0f
joshtin2505 <justincastro2505@gmail.com>
2024-02-25 19:45:37 -0500

    Add CORS and morgan middleware

--- src/app.ts
+++ src/app.ts
@@ -1,9 +1,13 @@
 import express from 'express'
+import cors from 'cors'
+import morgan from 'morgan'
 import noteRoutes from './routes/Harmony.routes'
 import RandomizerRoutes from './routes/Randomizer.routes'
 
 const app = express()
 
+app.use(cors())
+app.use(morgan('dev'))
 app.use(express.json())
 app.use('/api/random', RandomizerRoutes)
 app.use('/api', noteRoutes)
